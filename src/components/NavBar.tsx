97f6261d60c351d343ae34bf8538cb4b312f7624
debsdamaso <deboracert@gmail.com>
2024-04-17 19:35:23 -0300

    Mudança de símbolo no navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,30 +6,33 @@ interface NavBarProps {
 
 export function NavBar(props: NavBarProps){
     const active = props.active;
-    const classActive = "border-b-2 border-red-600 pb-1";
 
-    return(
-        <nav className="flex justify-between items-center px-8 py-4 bg-transparent w-full">
+    return (
+        <nav className="flex justify-between items-center px-8 py-6 bg-transparent w-full">
             <h1 className="text-4xl font-bold">SafeDose</h1>
-            <ul className="flex gap-16">
-                <li className={active === 'usuario' ? classActive : ""}>
-                    <Link href="/usuario">Usuário</Link>
+            <ul className="flex gap-16 items-center">
+                <li className={active === 'usuario' ? "flex items-center space-x-2" : "flex items-center"}>
+                    {active === 'usuario' && <span className="text-red-500 font-bold">+</span>}
+                    <Link href="/usuario">usuario</Link>
                 </li>
-                <li className={active === 'medicamentos' ? classActive : ""}>
-                    <Link href="/medicamentos">Medicamentos</Link>
+                <li className={active === 'medicamentos' ? "flex items-center space-x-2" : "flex items-center"}>
+                    {active === 'medicamentos' && <span className="text-red-500 font-bold">+</span>}
+                    <Link href="/medicamentos">medicamentos</Link>
                 </li>
-                <li className={active === 'funcionarios' ? classActive : ""}>
-                    <Link href="/funcionarios">Funcionários</Link>
+                <li className={active === 'funcionarios' ? "flex items-center space-x-2" : "flex items-center"}>
+                    {active === 'funcionarios' && <span className="text-red-500 font-bold">+</span>}
+                    <Link href="/funcionarios">funcionários</Link>
                 </li>
             </ul>
             <div className="flex items-center bg-white rounded-full px-2 py-2">
                 <input type="text" placeholder="Pesquisar" className="outline-none bg-transparent" />
-                {/* adicionar um ícone de pesquisar */}
+                {/* ícone de pesquisar */}
             </div>
         </nav>
-    )
+    );
 }
 
+export default NavBar;
 
 
 
