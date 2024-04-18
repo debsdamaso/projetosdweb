b5e9affc78489894b1e7ddce3ee7e6432fb8970e
debsdamaso <deboracert@gmail.com>
2024-04-17 22:29:07 -0300

    Adicionado Logo e ícone

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,7 +9,12 @@ export function NavBar(props: NavBarProps){
 
     return (
         <nav className="flex justify-between items-center px-8 py-6 bg-transparent w-full">
-            <h1 className="text-4xl font-bold">SafeDose</h1>
+            {/* Logo e Nome */}
+            <div className="flex items-center space-x-4">
+                <img src="/Logo.png" alt="SafeDose Logo" className="h-10 w-10" />
+                <h1 className="text-4xl font-bold">SafeDose</h1>
+            </div>
+            
             <ul className="flex gap-16 items-center">
                 <li className={active === 'usuario' ? "flex items-center space-x-2" : "flex items-center"}>
                     {active === 'usuario' && <span className="text-red-500 font-bold">+</span>}
@@ -25,14 +30,15 @@ export function NavBar(props: NavBarProps){
                 </li>
             </ul>
             <div className="flex items-center bg-white rounded-full px-2 py-2">
-                <input type="text" placeholder="Pesquisar" className="outline-none bg-transparent" />
-                {/* ícone de pesquisar */}
+                <input type="text" placeholder=" Pesquisar" className="outline-none bg-transparent" />
+                <img src="/search.png" alt="lupa" className="ml-2 h-6 w-6" />
             </div>
         </nav>
     );
 }
 
 export default NavBar;
+
 
 
 
